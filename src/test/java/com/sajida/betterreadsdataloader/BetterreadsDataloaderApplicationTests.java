d1eb6934169078c677b5b41fc6214d81e990d137
Sajida Tabasum <sajida.tabasum@phenompeople.com>
2022-04-24 21:12:12 +0530

    initial commit

---
+++ src/test/java/com/sajida/betterreadsdataloader/BetterreadsDataloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajida.betterreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsDataloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
